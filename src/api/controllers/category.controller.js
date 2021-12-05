9075c2c5cb0ebbe3b507786495fc19d2121c0ed4
williamdampier <74879032+williamdampier@users.noreply.github.com>
2021-12-06 10:00:26 +1300

    category control refactor

--- src/api/controllers/category.controller.js
+++ src/api/controllers/category.controller.js
@@ -6,100 +6,70 @@ const {models} = require('../../db')
 class CategoryController {
     async createCategory(req,res) {
         const {title, section_id} = req.body;
-       
-  
-        try {
-            const newCategory = await models.Category.create({title: title, section_id: section_id})
-                res.json(newCategory.dataValues)   
-        }         
-        catch (err) { 
-            const errObj = {};
-                err.errors.map( er => {
-                errObj[er.path] = er.message;
-            })
-            res.json(errObj);
-        }
+
+        await models.Category.create({title: title, section_id: section_id})
+        .then((category) => res.json(category)) 
+        .catch((err) => res.json(err.errors[0].message))      
     }
 
     async getCategories(req,res) {
         const section = req.query.section
-        if (section) {
-               
+
+        if (section) { 
                     await models.Category.findAll({
                         where: {section_id: section}
                     })
                     .then((categories)=> categories.length ? res.json(categories) : res.json("No valid items or section does not exist"))
-                    .catch((err) =>  res.json(err));
-                
-                
+                    .catch((err) =>  res.json(err.errors[0].message));
             }       
-
-       else { try {
-            const categories = await models.Category.findAll();
-            res.json(categories);
-        }
-        catch (err) { 
-            const errObj = {};
-                err.errors.map( er => {
-                errObj[er.path] = er.message;
-            })
-            res.json(errObj);
-        }  
-        
+       else { 
+            await models.Category.findAll()
+            .then((categories) => res.json(categories))
+            .catch((err) =>  res.json(err.errors[0].message));
+        }        
     }
-}
+
 
     async getCategoryById(req,res) {
-        const id = req.params.id;
-        const category = await models.Category.findByPk(id);
-        if (category) {res.json(category);  }
-        else  {res.json(`Category with id: ${id} does not exist`)}
-               
+       const id = req.params.id;
+       await models.Category.findByPk(id)
+       .then((category) => category ? res.json(category) : res.json(`Category with id: ${id} does not exist`))
+       .catch((err) =>  res.json(err));                
     }
+               
+    
 
     async updateCategory(req,res) {
         const id = req.params.id;
         const {title} = req.body; 
 
-        try {
-            await models.Category.update({title}, {
+        await models.Category.update({title}, {
                 where: {
                     id: id
                 }
             })
-            res.json(`Category with id: ${id} succesfully updated!`)
-           }
-           catch (err) { 
-            const errObj = {};
-                err.errors.map( er => {
-                errObj[er.path] = er.message;
-            })
-            res.json(errObj);
-        } 
+            .then(() => res.json(`Category with id: ${id} succesfully updated!`))
+            .catch((err) => res.status(500).json(err.errors[0].message))
+           
 };
 
     async deleteCategory(req,res) {
         const id = req.params.id;  
-        try {
-            const category = await models.Category.findByPk(id);
-            if (!category) {
-                res.json (`Category with ${id} does not exist`)
+        const category = await models.Category.findByPk(id);
+        
+        if (category) {
+            await models.Category.destroy({
+                where: {
+                    id: id
+                }})
+            .then (() => res.json(`User with id: ${id} succesfully deleted!`))
+            .catch((err) =>  res.json(err));
+                
             }
             else {
-                await models.Category.destroy({
-                    where: {
-                        id: id
-                    }})
-                res.json(`User with id: ${id} succesfully deleted!`)
+                res.json (`Category with ${id} does not exist`)
             }                  
-        }   
-        catch (err) { 
-            const errObj = {};
-                err.errors.map( er => {
-                errObj[er.path] = er.message;
-            })
-            res.json(errObj);
-        }   
+        
         
     }
 }
