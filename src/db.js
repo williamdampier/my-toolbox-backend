611886996484ee1c09a9ea652b0bb51cf0c99bf1
williamdampier <74879032+williamdampier@users.noreply.github.com>
2022-01-31 15:13:56 +1300

    oprions

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`) 
+const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, { rejectUnauthorized: false }) 
 
 
 const modelDefiners = [
